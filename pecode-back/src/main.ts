0f76923fd49951e1e9154106925e0306ba8a5a1f
Olexandr25 <oleksandrvoichyshyn@gmail.com>
2024-07-28 23:02:01 +0300

    chore: Update main.ts for Swagger documentation and port change

--- pecode-back/src/main.ts
+++ pecode-back/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +15,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
